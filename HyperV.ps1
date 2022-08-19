2a3c32351142b5ebbba53a2e2905806458fe96e2
Endre A Wik <none@none.com>
2022-08-19 13:06:09 +0200

    Create VMs

--- HyperV.ps1
+++ HyperV.ps1
@@ -1,5 +1,5 @@
 
-New-VM -Name 'A-ADCS1' -MemoryStartupBytes 1024 -Generation 2 -SwitchName (Get-VMSwitch -SwitchType Private) -
+
 
 
 
@@ -17,3 +17,33 @@ Set-VMHost -VirtualHardDiskPath 'C:\VHD' -VirtualMachinePath 'C:\HYPERV'
 # ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- #
 
 New-VMSwitch -Name 'PRIVATE' -SwitchType Private
+
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- #
+# Hyper-V - Create VMs
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- #
+
+$ISOPath = 'C:\ISO\en-us_windows_server_2022_x64_dvd_620d7eac.iso'
+$VHDPath = 'C:\VHD'
+
+function CreateVM ($VMName) {
+    $VM = New-VM -Name $VMName -Generation 2 -SwitchName (Get-VMSwitch -SwitchType Private).Name -NewVHDPath ($VHDPath + "\" + $VMName.ToLower() + "-c.vhdx") -NewVHDSizeBytes 100GB -BootDevice VHD
+    Set-VM -Name $VM.Name -ProcessorCount 4 -DynamicMemory -MemoryMinimumBytes 1024MB -MemoryMaximumBytes 8192MB
+    Add-VMDvdDrive -VMName $VM.Name -Path $ISOPath
+    $HostGuardianService = Get-HgsGuardian -Name UntrustedGuardian
+    $KeyProtector = New-HgsKeyProtector -Owner $HostGuardianService -AllowUntrustedRoot
+    Set-VMKeyProtector -VMName $VM.Name -KeyProtector $KeyProtector.RawData
+    Enable-VMTPM -VMName $VM.Name
+}
+
+$VMName = 'ADCS1'
+CreateVM $VMName
+$VMName = 'ADDS1'
+CreateVM $VMName
+$VMName = 'ADFS1'
+CreateVM $VMName
+$VMName = 'ADFS2'
+CreateVM $VMName
+$VMName = 'RAS1'
+CreateVM $VMName
+$VMName = 'WEB1'
+CreateVM $VMName
